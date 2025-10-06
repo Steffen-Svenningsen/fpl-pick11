00a90b83a3a5f2c4b947bd281349cc6f6d50ced2
Steffen-Svenningsen <stef7625@gmail.com>
2025-10-06 11:13:31 +0200

    feat: create leaderboard page

--- routes/web.php
+++ routes/web.php
@@ -7,9 +7,16 @@ Route::get('/', function () {
     return Inertia::render('Welcome');
 })->name('home');
 
-Route::get('dashboard', function () {
-    return Inertia::render('Dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+Route::middleware(['auth', 'verified'])->group(function () {
+    Route::get('dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+
+    Route::get('leaderboard', function () {
+        return Inertia::render('Leaderboard');
+    })->name('leaderboard');
+});
+
 
 require __DIR__.'/settings.php';
 require __DIR__.'/auth.php';
